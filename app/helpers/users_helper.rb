c773240ce40be6a4486339ae41e4cd868653a91a
luwei <luwei@luwei-Latitude-D610.(none)>
2014-08-27 18:46:29 +0800

    The first version with features: (1) bootstrap UI; (2) CRUD for job apps;

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,7 +2,13 @@ module UsersHelper
   # Returns the Gravatar (http://gravatar.com/) for the given user.
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}/?s=22"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
+  
+  def gravatar_for_large(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}/?s=100"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
-  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  match '/signout', to: 'sessions#destroy',     via: 'get'
   match '/newjob',  to: 'jobapps#new',          via: 'get'
 
   match '/help',    to: 'static_pages#help',    via: 'get'
