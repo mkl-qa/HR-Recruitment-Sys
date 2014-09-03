af09e819ab9a80a60a5a23e77226e013b083c73b
Grant Cai <cfc.fchen@gmail.com>
2014-09-03 15:47:20 +0800

    commit data dictionary initial version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,20 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  def set_parent_id(pid)
+    cookies.permanent[:diction_parent_id] = pid
+  end
+  def get_parent_id
+    cookies.permanent[:diction_parent_id]
+  end
+  
+  def set_max_id(mid)
+    cookies.permanent[:diction_max_id] = mid
+  end
+  def get_max_id
+    cookies.permanent[:diction_max_id]
+  end  
   
 
 end
---
+++ app/views/data_dictionaries/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @data_dictionary, :id, :name, :description, :parent_code, :del_flg, :created_at, :updated_at
