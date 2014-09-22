b50544378ed96037d2df0766b20391d5215b0407
alexwang1 <awang@.pclc0.merkle.local>
2014-09-22 15:16:59 +0800

    alex-retored

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140905061000) do
+ActiveRecord::Schema.define(version: 20140912030837) do
 
   create_table "data_dictionaries", force: true do |t|
     t.string   "name"
